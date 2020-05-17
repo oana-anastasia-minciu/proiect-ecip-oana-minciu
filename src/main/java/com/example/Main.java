f929b5e0fdcedc7eade39f9fc3b3c2f700ac2b94
oana-anastasia-minciu <oana.anastasia.minciu@gmail.com>
2020-05-17 15:32:35 +0000

    Added greeting form

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -28,6 +28,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.sql.DataSource;
@@ -38,9 +39,12 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Map;
 
+import org.springframework.ui.Model;
+
 @Controller
 @SpringBootApplication
-public class Main {
+public class Main 
+{
 
   @Value("${spring.datasource.url}")
   private String dbUrl;
@@ -48,47 +52,71 @@ public class Main {
   @Autowired
   private DataSource dataSource;
 
-  public static void main(String[] args) throws Exception {
+  public static void main(String[] args) throws Exception 
+  {
     SpringApplication.run(Main.class, args);
   }
 
-  @RequestMapping("/")
-  String index() {
+  @RequestMapping("/")      // map the home of the website
+  String index() 
+  {
     return "index";
   }
-  @RequestMapping("/hello")
-  String hello(Map<String, Object> model) {
+
+  @RequestMapping("/hello")   // map the hello page of the website
+  String hello(Map<String, Object> model) 
+  {
       RelativisticModel.select();
-      Amount<Mass> m = Amount.valueOf("12 GeV").to(KILOGRAM);
-      model.put("science", "E=mc^2: 12 GeV = " + m.toString());
+      String energy = System.getenv().get("ENERGY");
+      if (energy == null) {
+         energy = "12 GeV";
+      }
+      Amount<Mass> m = Amount.valueOf(energy).to(KILOGRAM);
+      model.put("science", "E=mc^2: " + energy + " = "  + m.toString());  // print something in science variable
       return "hello";
   }
+
+  @GetMapping("/greeting")
+  public String greetingForm(Model model) 
+  {
+    model.addAttribute("greeting", new Greeting());
+    return "greeting";
+  }
   @RequestMapping("/db")
-  String db(Map<String, Object> model) {
-    try (Connection connection = dataSource.getConnection()) {
+  String db(Map<String, Object> model) 
+  {
+    try (Connection connection = dataSource.getConnection()) 
+    {
       Statement stmt = connection.createStatement();
       stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
       stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
       ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
 
       ArrayList<String> output = new ArrayList<String>();
-      while (rs.next()) {
+      while (rs.next()) 
+      {
         output.add("Read from DB: " + rs.getTimestamp("tick"));
       }
 
       model.put("records", output);
       return "db";
-    } catch (Exception e) {
+    } 
+    catch (Exception e) 
+    {
       model.put("message", e.getMessage());
       return "error";
     }
   }
 
   @Bean
-  public DataSource dataSource() throws SQLException {
-    if (dbUrl == null || dbUrl.isEmpty()) {
+  public DataSource dataSource() throws SQLException
+  {
+    if (dbUrl == null || dbUrl.isEmpty())               // check database url
+    {
       return new HikariDataSource();
-    } else {
+    } 
+    else 
+    {
       HikariConfig config = new HikariConfig();
       config.setJdbcUrl(dbUrl);
       return new HikariDataSource(config);
