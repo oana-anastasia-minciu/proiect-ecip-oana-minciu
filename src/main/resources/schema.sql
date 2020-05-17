56dca394e655c7c7296743ee18126db34a3a902e
oana-anastasia-minciu <oana.anastasia.minciu@gmail.com>
2020-05-17 21:48:03 +0000

    More db stuff

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,31 +1,3 @@
-
-
-/*create table users
-(
-    `id`       bigint(11) unsigned NOT NULL AUTO_INCREMENT,
-    `username` varchar(100) not null,
-    `password` varchar(100) not null,
-    `enabled`  boolean     not null,
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `username_unique` (`username`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4;
-
-create table user_authorities
-(
-    `id`        bigint(11) unsigned NOT NULL AUTO_INCREMENT,
-    `user_id`   bigint(11) unsigned NOT NULL,
-    `authority` varchar(50) not null,
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `username_authorities_unique` (`user_id`, `authority`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4;
-
-ALTER TABLE `user_authorities`
-    ADD CONSTRAINT `fk_authorities`
-        FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
-*/
-
 DROP TABLE IF EXISTS employee;
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS authorities;
